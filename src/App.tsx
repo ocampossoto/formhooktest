c3f2dd25483386b9e6389f6a9555cbab6c4ecd52
Oscar Campos-Soto <ocampossoto1@gmail.com>
2024-01-11 09:00:14 -0600

    Adding form example

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { TextField } from "@mui/material";
+const total = 1000;
+function getNames() {
+  let data = [];
+  for (let i = 0; i < total; i++) {
+    data.push(`Name-${i}`);
+  }
+  return data;
+}
+function getObject(namesData: any) {
+  let schemaObject: any = {};
+  for (let name of namesData) {
+    schemaObject[name] = yup.string();
+  }
+  return schemaObject;
+}
 
 function App() {
+  const namesList = getNames();
+  const schemaTemp = getObject(namesList);
+  const schema1 = yup.object(schemaTemp).required();
+  type FormData = yup.InferType<typeof schema1>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(schema1),
+  });
+  const onSubmit = (data: FormData) => console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {namesList.map((name) => (
+          <div>
+            <TextField variant="outlined" key={name} {...register(name)} />
+            <br />
+          </div>
+        ))}
+
+        <input type="submit" />
+      </form>
     </div>
   );
 }
